9363e8ee033f48dd6d5b3eeda66fa926b3f1d49e
Branko Bozovic <branko028@yahoo.com>
2019-02-20 17:25:05 +0100

    Added all animation, formspree for form send

--- main.js
+++ main.js
@@ -12,19 +12,26 @@ let isValidFirst;
 let isValidLast;
 let isValidEmail;
 
+let percent = 0;
+let elem = document.getElementById('percent-complete');
+
 firstName.addEventListener('click', function() {
     document.querySelector('#animate-1 > div').classList.add('animation-border');
 })
 
 firstName.addEventListener('focusout', function() {
+    setTimeout(function() {
 
-    document.querySelector('#animate-1 > div').classList.remove('animation-border');  
+        document.querySelector('#animate-1 > div').classList.remove('animation-border');
+        if ( isValidFirst = firstName.checkValidity() ) {
+            document.querySelector('#animate-1').classList.add('animation-styles');
+            document.querySelector('#animate-1 > div + span').innerHTML = "&#10004;";
+            document.querySelector('.form-title .animation-circle > div').classList.add('border-top');
+            percent += 25;
+            elem.innerHTML = percent + '%';
+        }
 
-    if ( isValidFirst = firstName.checkValidity() ) {
-        document.querySelector('#animate-1').classList.add('animation-styles');
-        document.querySelector('#animate-1 > div + span').innerHTML = "&#10004;";
-    }
-
+    }, 1000);
 })
   
 
@@ -33,12 +40,17 @@ lastName.addEventListener('click', function() {
 })
 
 lastName.addEventListener('focusout', function() {
-    document.querySelector('#animate-2 > div').classList.remove('animation-border');
+    setTimeout(function() {
 
-    if ( isValidLast = lastName.checkValidity() ) {
-        document.querySelector('#animate-2').classList.add('animation-styles');
-        document.querySelector('#animate-2 > div + span').innerHTML = "&#10004;";
-    }
+        document.querySelector('#animate-2 > div').classList.remove('animation-border');
+        if ( isValidLast = lastName.checkValidity() ) {
+            document.querySelector('#animate-2').classList.add('animation-styles');
+            document.querySelector('#animate-2 > div + span').innerHTML = "&#10004;";
+            document.querySelector('.form-title .animation-circle > div').classList.add('border-right');
+            percent += 25;
+            elem.innerHTML = percent + '%';
+        }
+    }, 1000);  
 })
 
 
@@ -47,29 +59,41 @@ emailField.addEventListener('click', function() {
 })
 
 emailField.addEventListener('focusout', function() {
-    document.querySelector('#animate-3 > div').classList.remove('animation-border');
-
-    if ( isValidEmail = emailField.checkValidity() ) {
-        document.querySelector('#animate-3').classList.add('animation-styles');
-        document.querySelector('#animate-3 > div + span').innerHTML = "&#10004;";
-    }
+    setTimeout(function() {
+        document.querySelector('#animate-3 > div').classList.remove('animation-border');
+        if ( isValidEmail = emailField.checkValidity() ) {
+            document.querySelector('#animate-3').classList.add('animation-styles');
+            document.querySelector('#animate-3 > div + span').innerHTML = '&#10004;';
+            document.querySelector('.form-title .animation-circle > div').classList.add('border-bottom');
+            percent += 25;
+            elem.innerHTML = percent + '%';
+        }
+    }, 1000);
 })
 
 
-radioMonday.addEventListener('click', function() {
-    document.querySelector('#animate-4').classList.add('animation-styles');
-    document.querySelector('#animate-4 > div + span').innerHTML = "&#10004;";
+radioMonday.addEventListener('change', function() {
+    setTimeout(function() {
+        document.querySelector('#animate-4').classList.add('animation-styles');
+        document.querySelector('#animate-4 > div + span').innerHTML = '&#10004;';
+        document.querySelector('.form-title .animation-circle > div').classList.add('border-left');
+        percent += 25;
+        elem.innerHTML = percent + '%';
+    }, 1000);
 })
-radioTuesday.addEventListener('click', function() {
-    document.querySelector('#animate-4').classList.add('animation-styles');
-    document.querySelector('#animate-4 > div + span').innerHTML = "&#10004;";
+radioTuesday.addEventListener('change', function() {
+    setTimeout(function() {
+        document.querySelector('#animate-4').classList.add('animation-styles');
+        document.querySelector('#animate-4 > div + span').innerHTML = '&#10004;';
+        document.querySelector('.form-title .animation-circle > div').classList.add('border-left');
+    }, 1000);
 })
 
-  
+
 btn.addEventListener('click', function() {
     if ( isValidFirst && isValidLast && isValidEmail ) {
         contactForm.submit();
     } else {
-        alert("Please fill all fields");
+        alert('Please fill all fields');
     }
 });
